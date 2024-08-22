2983f1f8f583f7843aa857b1fcd080fe7f66329a
akash.rawat@arthmate.co.in-at-172131073603 <akash.rawat@arthmate.co.in>
2024-08-22 12:45:07 +0530

    Initial commit

---
+++ app/login.tsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, Alert } from 'react-native'
+
+export default function SignUpView() {
+
+  const [fullName, setFullName] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <Image
+          style={styles.inputIcon}
+          source={{ uri: 'https://img.icons8.com/ios-glyphs/512/user-male-circle.png' }}
+        />
+        <TextInput
+          style={styles.inputs}
+          placeholder="Full name"
+          keyboardType="email-address"
+          underlineColorAndroid="transparent"
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Image
+          style={styles.inputIcon}
+          source={{ uri: 'https://img.icons8.com/ios-filled/512/circled-envelope.png' }}
+        />
+        <TextInput
+          style={styles.inputs}
+          placeholder="Email"
+          keyboardType="email-address"
+          underlineColorAndroid="transparent"
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Image
+          style={styles.inputIcon}
+          source={{ uri: 'https://img.icons8.com/ios-glyphs/512/key.png' }}
+        />
+        <TextInput
+          style={styles.inputs}
+          placeholder="Password"
+          secureTextEntry={true}
+          underlineColorAndroid="transparent"
+        />
+      </View>
+      <TouchableOpacity
+        style={[styles.buttonContainer, styles.signupButton]}>
+        <Text style={styles.signUpText}>Sign up</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00b5ec',
+    width: '100%',
+  },
+  inputContainer: {
+    borderBottomColor: '#F5FCFF',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 30,
+    borderBottomWidth: 1,
+    width: 250,
+    height: 45,
+    marginBottom: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  inputs: {
+    height: 45,
+    marginLeft: 16,
+    borderBottomColor: '#FFFFFF',
+    flex: 1,
+  },
+  inputIcon: {
+    width: 30,
+    height: 30,
+    marginLeft: 15,
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+  },
+  signupButton: {
+    backgroundColor: '#FF4DFF',
+  },
+  signUpText: {
+    color: 'white',
+  },
+})
